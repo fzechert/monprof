367614c3f4d713766995c5e61d3e12218a7bcab7
Frank Zechert <frank@zechert.net>
2023-11-12 03:59:42 +0100

    initialize logging

--- monprof/src/main.rs
+++ monprof/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use log::{trace, LevelFilter};
+
+fn init_logging(verbose: bool, quiet: bool, json_output: bool) {
+    let mut level = LevelFilter::Info;
+
+    if quiet || json_output {
+        level = LevelFilter::Off;
+    } else if verbose {
+        level = LevelFilter::Trace;
+    }
+
+    env_logger::builder().filter(None, level).init();
+    trace!("Logging system initialized");
+}
+
 }
